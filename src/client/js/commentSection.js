bc552d46447178352a2b97f998a82aa56ab8307f
HiImConan <mindol0320@naver.com>
2022-02-07 21:55:22 +0900

    Fixed local comment activation
    cleaned code using basic JS element function

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -7,29 +7,34 @@ const deleteComment = document.getElementById("deleteComment");
 
 const addComment = (text, id) => {
   // constructing html element with JS not pug
-  const videoComments = document.querySelector(".video__comments ul");
+  // 댓글 리스트 엘리먼트 불러오기
+  const videoCommentsList = document.querySelector(".video__comments ul");
+  // 새로운 코멘트 엘리먼트 생성하기V41010101
   const newComment = document.createElement("li");
+  // 새로운 코멘트의 주소는 이 비디오의 주소와 동일
   newComment.dataset.id = id;
+  // 기존 코멘트 클래스 네임과 같은 네임 부여
   newComment.className = "video__comment";
 
-  /*
+  // 유저 아바타 섹션 구현하기
   const userAvatarDiv = document.createElement("div");
   userAvatarDiv.className = "video__comment-avatar";
   const userAvatarImg = document.createElement("img");
-  const imgSrc = document.querySelector("imgsrc");
-  userAvatarImg.srcset = `${imgSrc.src}`;
+  const imgSrc = document.querySelector("#imgsrc");
+  userAvatarImg.src = `${imgSrc.src}`;
   userAvatarImg.crossOrigin = "crossorigin";
   userAvatarDiv.appendChild(userAvatarImg);
-  newComment.appendChild(userAvatarDiv);
-  */
 
   const commentTextareaDiv = document.createElement("div");
   commentTextareaDiv.className = "video__comment-textarea";
   const usernameSpan = document.createElement("span");
+  usernameSpan.className = ".video__comment-username";
   usernameSpan.innerText = "걍해";
   const textSpan = document.createElement("span");
+  textSpan.className = ".video__comment-textbox";
   textSpan.innerText = `${text}`;
-  commentTextarea.appendChild(usernameSpan, textSpan);
+  commentTextareaDiv.appendChild(usernameSpan);
+  commentTextareaDiv.appendChild(textSpan);
 
   const deleteButtonDiv = document.createElement("div");
   deleteButtonDiv.className = "video__comment-buttons";
@@ -37,9 +42,10 @@ const addComment = (text, id) => {
   deleteButton.className = "fas fa-trash-alt";
   deleteButtonDiv.appendChild(deleteButton);
 
+  newComment.appendChild(userAvatarDiv);
   newComment.appendChild(commentTextareaDiv);
   newComment.appendChild(deleteButtonDiv);
-  videoComments.prepend(newComment); // append - at the end, prepend - in front
+  videoCommentsList.prepend(newComment); // append - at the end, prepend - in front
 };
 
 const handleSubmit = async (event) => {
